2935d377bb52a2f8491599ad4926c094370f0f73
KamiyaNya <dikriss16@gmail.com>
2023-11-29 14:54:57 +0300

    метаданные

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,5 +1,10 @@
 import ChatPage from '@/components/screens/Chat/Chat.page';
 
+export const metadata = {
+	title: 'Chat Page',
+	description: 'Chat page',
+};
+
 export default function Chat() {
 	return <ChatPage />;
 }
